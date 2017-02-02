d266534b01d20684c36577ee310881d6d20aea34
uday wakte <uday.wakte@gmail.com>
2017-02-02 12:33:29 +0000

    timestamp service 18:00 170202

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+var moment = require('moment');
 
 app.get('/', function(req,res){
-    res.end('Hello Worasdld!');
+    //res.end('Hello Worasdld!');
+    res.sendFile(path.join(__dirname+'/index.html'));
 });
+
+app.get('/:ds', function(req,res){
+    var ds = req.params.ds;
+    //var date = {unix: '', natural: ''};
+    var unix = null;
+    var natural = null;
+    if(+ds>0){
+        unix = +ds;
+        natural = moment(+ds, 'X').format('MMMM D, YYYY');
+    }
+    else if (isNaN(+ds) && moment(ds, 'MMMM D, YYYY').isValid()){
+        unix = moment(ds, 'MMMM D, YYYY').format('X');
+        natural = ds;
+    }
+    
+    var result = {unix: unix, natural: natural};
+    
+    res.send(result);
+    
+});
+
+
 app.listen(8080, function(){
     console.log('port is 8080!');
 });
